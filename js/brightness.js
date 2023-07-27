f006de8f90953644168f7014448d406e8d4412e1
nhyxx <leehyeoilin@gmail.com>
2023-07-27 16:43:22 +0900

    backgroundChange

--- js/brightness.js
+++ js/brightness.js
@@ -1,7 +1,25 @@
-document.getElementById("brightnessbutton").addEventListener("click", changeBrightness);
+const button = document.getElementById('brightenessbutton');
+
+button.addEventListener('click', changeBrightness)
 
 function changeBrightness() {
-    const background = document.background;
-    const currentBrightness = getComputedStyle(background).getPropertyValue('--brightness');
-    
-}
+    let brightness = getComputedStyle(document.documentElement).getPropertyValue('--brightness');
+    if (brightness === '100%') {
+        brightness = '75%';
+        button.innerHTML = '75%';  
+    } 
+    else if (brightness === '75%') {
+        brightness = '50%';
+        button.innerHTML = '50%';  
+    } 
+
+    else if (brightness === '50%') {
+        brightness = '25%';
+        button.innerHTML = '25%';  
+    } 
+        else {
+        brightness = '100%';
+        button.innerHTML = '100%'; 
+    }
+    document.documentElement.style.setProperty('--brightness', brightness);
+};
